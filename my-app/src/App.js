076433b1ada914e1ae3cf7e17c723154b1f88d9e
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-18 16:06:59 +0000

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+
+function App() {
+  return <div className="App">
+      <MethodsAsPropsParent />
+  </div>;
+}
+
+export default App;
